f2f2fa53486a2271fef302aa7855acbbb7020dcb
Steven Tran <steven.tran077@gmail.com>
2023-12-01 20:57:08 -0800

    Day 1 task1 completed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use axum::{routing::get, Router};
+
+use axum::{routing::get, Router,
+        extract::Path};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
-
+async fn task1(Path((num1, num2)): Path<(i32, i32)>) -> String{
+    (num1 ^ num2).pow(3).to_string()
+}
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new().route("/", get(hello_world))
+                            .route("/1/:num1/:num2", get(task1));
 
     Ok(router.into())
 }
